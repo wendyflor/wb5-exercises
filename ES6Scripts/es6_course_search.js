420fede7c8961fc50ce3cb0b1ab29ad2b2bef6da
Wendy Flores <wendyf@appdev.yearup.org>
2023-05-15 14:49:06 -0400

    First commit

---
+++ ES6Scripts/es6_course_search.js
@@ -0,0 +1,47 @@
+let courses = [
+  {
+    CourseId: "PROG100",
+    Title: "Introduction to HTML/CSS/Git",
+    Location: "Classroom 7",
+    StartDate: "09/08/22",
+    Fee: "100.00",
+  },
+  {
+    CourseId: "PROG200",
+    Title: "Introduction to JavaScript",
+    Location: "Classroom 9",
+    StartDate: "11/22/22",
+    Fee: "350.00",
+  },
+  {
+    CourseId: "PROG300",
+    Title: "Introduction to Java",
+    Location: "Classroom 1",
+    StartDate: "01/09/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROG400",
+    Title: "Introduction to SQL and Databases",
+    Location: "Classroom 7",
+    StartDate: "03/16/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROJ500",
+    Title: "Introduction to Angular",
+    Location: "Classroom 1",
+    StartDate: "04/25/23",
+    Fee: "50.00",
+  },
+];
+function findPROG200(course) {
+  return course.CourseId === "PROG200";
+}
+
+const prog200 = courses.find(findPROG200);
+console.log("Prog200 starts", prog200.StartDate);
+
+//function courseStart(courses) {
+//return
+//}
